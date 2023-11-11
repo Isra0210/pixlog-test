1940a65a1ba43afb727b69f691f6b0d1f924503c
Israel Rodrigues <israelrodrigues@alunos.utfpr.edu.br>
2023-11-11 02:45:33 -0300

    updated implementation storage helper

--- lib/helper/storage_helper.dart
+++ lib/helper/storage_helper.dart
@@ -1,33 +1,43 @@
 import 'package:localstorage/localstorage.dart';
+import 'package:pixlogtest/helper/enum_helper.dart';
 
 class StorageHelper {
   StorageHelper();
 
-  final LocalStorage storage = LocalStorage('pixlogtest');
+  final LocalStorage storage = LocalStorage('pixlog');
 
-  Future<List> getResourcesByLanguageId(
-    String languageId,
-  ) async {
+  Future<List>? getListFromStorage({required StorageKeysEnum key}) async {
     await storage.ready;
-    final List resources = await storage.getItem(languageId);
-    return resources;
-  }
-
-  Future<void> saveResourceListByLanguageId(
-    Map<String, List<Map<String, dynamic>>> resource,
-  ) async {
-    await storage.ready;
-    await storage.setItem(resource.keys.first, resource[resource.keys.first]);
-  }
-
-  Future saveLanguageList(List<String> languages) async {
-    await storage.ready;
-    await storage.setItem('languages', languages);
-  }
-
-  Future<List>? getLanguageList() async {
-    await storage.ready;
-    final List? resources = await storage.getItem('languages');
+    final List? resources = await storage.getItem(key.name);
     return resources ?? [];
+  }
+
+  Future saveListToStorage({
+    required List list,
+    required StorageKeysEnum key,
+  }) async {
+    await storage.ready;
+    await storage.setItem(key.name, list);
+  }
+
+  Future<void> saveMapToStorage({
+    required Map<String, List<Map<String, dynamic>>> map,
+  }) async {
+    await storage.ready;
+    await storage.setItem(map.keys.first, map[map.keys.first]);
+  }
+
+  Future<String?> getStringFromStorage({required StorageKeysEnum key}) async {
+    await storage.ready;
+    final String? isFirstTime = await storage.getItem(key.name);
+    return isFirstTime;
+  }
+
+  Future<void> saveStringToStorage({
+    required String key,
+    required String value,
+  }) async {
+    await storage.ready;
+    await storage.setItem(key, value);
   }
 }
