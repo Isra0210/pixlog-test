a2984b2ea5429243ca9de941a7c6ede2c906e9f0
Israel Rodrigues <israelrodrigues@alunos.utfpr.edu.br>
2023-11-11 02:44:30 -0300

    created filter page

--- lib/helper/router_helper.dart
+++ lib/helper/router_helper.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
-import '../app/view/home_page.dart';
+import '../app/filter/view/filter_list_view.dart';
+import '../app/resource/view/home_page.dart';
 
 class RouterHelper {
   static Map<String, Widget Function(BuildContext)> get routes => {
         HomeView.route: (context) => const HomeView(),
+        FilterListView.route: (context) => const FilterListView(),
       };
 }
